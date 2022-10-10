1f85d82f19f1f438f57da6b5dd8a3240606ee6fe
Francesca-Barcella <pollon.fra@gmail>
2022-10-10 14:18:06 +0200

    FB - calcolo e stamp multipli 3  + multipli 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,16 +6,23 @@ e per i multipli di 5 stampi “Buzz”.
 Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. 
 */
 
+
+
 // 1. stampare in console i numeri da 1 a 100
-for (let i=1; i<=100; i++){
+for (let i = 1; i <= 100; i++) {
     console.log(i);
+    // 2. per i multipli SOLO di 3 far apparire la stringa "Fizz"
+    //2.1 come capire se un numero (tra 1 e 100) è multiplo di 3 ?
+    // un numero è multiplo di 3 se questo da 0 come resto dopo essere statao diviso per 3.
+    const multiplo3 = (i % 3 == 0);
+    // 3. per i multipli SOLO di 5 far apparire la string "Buzz"
+    //3.1 come capire se un numero (tra 1 e 100) è multiplo di 5?
+    const multiplo5 = (i % 5 == 0);
+    console.log(multiplo3, multiplo5);
+
 }
 
-// 2. per i multipli SOLO di 3 far apparire la stringa "Fizz"
-    //2.1 come capire se un numero (tra 1 e 100) è multiplo di 3 ?
 
-// 3. per i multipli SOLO di 5 far apparire la string "Buzz"
-    //3.1 come capire se un numero (tra 1 e 100) è multiplo di 5?
 
 // 4. per i multipli SIA di 3 SIA di 5 far apparire la stringa "FizzBuzz"
     //4.1 come capire se un numero (tra 1 e 100) è multiplo di 3 e di 5?
