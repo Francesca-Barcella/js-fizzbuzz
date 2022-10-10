7d1faa4cc5468e606932711a4158357a5831941c
Francesca-Barcella <pollon.fra@gmail>
2022-10-10 14:22:30 +0200

    FB - calcoloro e stamp multipli 3e5

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,17 @@ for (let i = 1; i <= 100; i++) {
     //2.1 come capire se un numero (tra 1 e 100) è multiplo di 3 ?
     // un numero è multiplo di 3 se questo da 0 come resto dopo essere statao diviso per 3.
     const multiplo3 = (i % 3 == 0);
+    console.log('il numero è multiplo di 3:', multiplo3);
     // 3. per i multipli SOLO di 5 far apparire la string "Buzz"
     //3.1 come capire se un numero (tra 1 e 100) è multiplo di 5?
     const multiplo5 = (i % 5 == 0);
-    console.log(multiplo3, multiplo5);
+    console.log('il numero è multiplo di 5:', multiplo5);
+    // 4. per i multipli SIA di 3 SIA di 5 far apparire la stringa "FizzBuzz"
+    //4.1 come capire se un numero (tra 1 e 100) è multiplo di 3 e di 5?
+    const multiplo3e5 = (i % 3 == 0 && i % 5 == 0);
+    console.log('il numero è multiplo sia di 3 sia di 5:', multiplo3e5);
 
 }
 
 
 
-// 4. per i multipli SIA di 3 SIA di 5 far apparire la stringa "FizzBuzz"
-    //4.1 come capire se un numero (tra 1 e 100) è multiplo di 3 e di 5?
