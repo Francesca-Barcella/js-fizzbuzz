91ce32ec7de51a5a8d8179283fe0153cfb7a7d3b
Francesca-Barcella <pollon.fra@gmail>
2022-10-10 16:14:53 +0200

    FBB - creato e appeso un titolo

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -14,34 +14,47 @@ for (let i = 1; i <= 100; i++) {
     //console.log(i);
     // 2. individuare i multipli di 3
     // 2.1 assegnare valore "Fizz" a i multipla di 3
-    let multiplo3 = (i % 3 == 0);
-    multiplo3 = 'Fizz'
+    const multiplo3 = 'Fizz';
     //console.log('il numero è multiplo di 3:', multiplo3);
     // 3. individuare i multipli di 5
     // 3.1 assegnare valore "Buzz" a i multipla di 5
-    let multiplo5 = (i % 5 == 0);
-    multiplo5 = 'Buzz'
+    const multiplo5 = 'Buzz';
     //console.log('il numero è multiplo di 5:', multiplo5);
     // 4. pindividuare i multipli di 3 e 5
     // 4.1 assegnare valore "FizzBuzz" a i multipla sia di 3 sia di 5
-    let multiplo3e5 = (i % 3 == 0 && i % 5 == 0);
-    multiplo3e5 = 'FizzBuz'
+    const multiplo3e5 = 'FizzBuz';
     //console.log('il numero è multiplo sia di 3 sia di 5:', multiplo3e5);
-    
+
     // 4. far apparire la stringa "Fizz" al posto dei multipli di 3 
-    if (i % 3 == 0){
-        console.log(multiplo3);
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(multiplo3e5);
         // 5. far apparire la stringa "Buzz" al posto dei multipli di 5
-    } else if (i % 5 == 0){
+    } else if (i % 5 == 0) {
         console.log(multiplo5);
         // 6. far apparire la stringa "FizzBuzz" al posto dei multipli sia di 3 sia di 5
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log(multiplo3e5);
+    } else if (i % 3 == 0) {
+        console.log(multiplo3);
         // 7. far apparire il numero se non è multiplo di niente
-    } else{
+    } else {
         console.log(i);
     }
 }
+
+/*
+BONUS 1:
+Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
+BONUS 2:
+Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+  */
+
+// bonu 1.1 - seleziono un elemento del dom
+const containerEL = document.querySelector('.container');
+// bonus 1.2 - creo riquadro per i risultati con la funzione append
+const titleDom = document.createElement('h1');
+// bonus 1.3 - appendo il contenuto all'elemento creato
+titleDom.append('FizzBuzzDOM');
+//bonus 1.4 - appendo il mio elemento al dom
+containerEL.append(titleDom);
 
 
 
