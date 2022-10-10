c4fb1a9261ab43748e0d2e5e9b560eec7b81b706
Francesca-Barcella <pollon.fra@gmail>
2022-10-10 13:59:34 +0200

    FB - stampati in console n° 1/100

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
-alert('ciao, funziono!')
+/* 
+TRACCIA
+Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. 
+*/
+
+// 1. stampare in console i numeri da 1 a 100
+for (let i=1; i<=100; i++){
+    console.log(i);
+}
+
+// 2. per i multipli SOLO di 3 far apparire la stringa "Fizz"
+    //2.1 come capire se un numero (tra 1 e 100) è multiplo di 3 ?
+
+// 3. per i multipli SOLO di 5 far apparire la string "Buzz"
+    //3.1 come capire se un numero (tra 1 e 100) è multiplo di 5?
+
+// 4. per i multipli SIA di 3 SIA di 5 far apparire la stringa "FizzBuzz"
+    //4.1 come capire se un numero (tra 1 e 100) è multiplo di 3 e di 5?
